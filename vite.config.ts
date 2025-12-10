d418d3e0944f921f05b2850aeb53d1c85fcf371d
Daniel Duff <daniel.duff@mantelgroup.com.au>
2025-12-10 16:35:58 +1000

    Clear out references to the legacy server code

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": "http://localhost:3000",
-      "/socket.io": {
-        target: "http://localhost:3000",
-        ws: true,
-      },
-    },
-  },
+
   build: {
     outDir: "dist",
     emptyOutDir: true,
